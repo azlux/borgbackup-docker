8a0d32f68f5c45a9a692fa93dd3b3f25043740b9
Azlux <github@azlux.fr>
2022-02-28 13:34:44 +0100

    change db name

--- script_backup.sh
+++ script_backup.sh
@@ -3,15 +3,15 @@
 source /tmp/project_env.sh
 
 if ! [ -z "$MYSQL_HOST" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ]; then
-    MYSQL_PWD="$MYSQL_PASSWORD" mysqldump --all-databases --single-transaction -u "$MYSQL_USER" -h "$MYSQL_HOST" > /tmp/all_databases.sql
-    borgbackup create --stats --compression lz4 "$BACKUP_PATH"::db_$(date +%Y-%m-%d_%H:%M) "/tmp/all_databases.sql"
-    rm /tmp/all_databases.sql
+    MYSQL_PWD="$MYSQL_PASSWORD" mysqldump --all-databases --single-transaction -u "$MYSQL_USER" -h "$MYSQL_HOST" > /tmp/all_databases_mysql.sql
+    borgbackup create --stats --compression lz4 "$BACKUP_PATH"::db_mysql_$(date +%Y-%m-%d_%H:%M) "/tmp/all_databases_mysql.sql"
+    rm /tmp/all_databases_mysql.sql
 fi
 
 if ! [ -z "$POSTGRES_HOST" ] || [ -z "$POSTGRES_USER" ] || [ -z "$POSTGRES_PASSWORD" ]; then
-    PGPASSWORD="$POSTGRES_PASSWORD" pg_dumpall -U "$POSTGRES_USER" -h "$POSTGRES_HOST" > /tmp/all_databases.sql
-    borgbackup create --stats --compression lz4 "$BACKUP_PATH"::db_$(date +%Y-%m-%d_%H:%M) "/tmp/all_databases.sql"
-    rm /tmp/all_databases.sql
+    PGPASSWORD="$POSTGRES_PASSWORD" pg_dumpall -U "$POSTGRES_USER" -h "$POSTGRES_HOST" > /tmp/all_databases_pg.out
+    borgbackup create --stats --compression lz4 "$BACKUP_PATH"::db_postgres_$(date +%Y-%m-%d_%H:%M) "/tmp/all_databases_pg.out"
+    rm /tmp/all_databases_pg.out
 fi
 
 for d in $FOLDERS_TO_BACKUP_PATH/*; do
