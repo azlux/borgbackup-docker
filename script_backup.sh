d71ade16f9a0dfdfed999c97c0000a1211136ce7
Azlux <github@azlux.fr>
2026-07-01 18:09:55 +0200

    better logs and function

--- script_backup.sh
+++ script_backup.sh
@@ -1,75 +1,127 @@
 #!/usr/bin/env bash
-echo $(date --iso-8601=seconds) STARTING BACKUP JOB
-test -f /tmp/project_env.sh && source /tmp/project_env.sh
+set -u
 
-# Monitoring KUMA with the Push monitor
-KUMA_PUSH_URL="${KUMA_PUSH_URL}"
+log() {
+    echo "$(date --iso-8601=seconds) $*"
+}
+
 BACKUP_ERRORS=0
 
-if ! ([ -z "$MYSQL_HOST" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ]); then
-    echo $(date --iso-8601=seconds) STARTING BACKUP MYSQL
-    MYSQL_PWD="$MYSQL_PASSWORD" mysqldump --all-databases --single-transaction -u "$MYSQL_USER" -h "$MYSQL_HOST" > /tmp/all_databases_mysql.sql
+handle_borg_rc() {
+    local rc="$1"
+    local operation="$2"
+
+    case "$rc" in
+        0)
+            ;;
+        1)
+            log "WARNING: ${operation} completed with warnings"
+            ;;
+        *)
+            log "ERROR: ${operation} failed (rc=${rc})"
+            BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
+            ;;
+    esac
+}
+
+backup_archive() {
+    local archive_name="$1"
+    local sources="$2"
+
+    borgbackup create --stats --compression lz4 "$BACKUP_PATH"::"$archive_name" "$sources"
+    handle_borg_rc $? "Backup ${archive_name}"
+}
+
+prune_archive() {
+    local pattern="$1"
+
+    borgbackup prune --stats -v --glob-archives="$pattern" --keep-within="$BORG_KEEP_WITHIN" --keep-weekly="$BORG_KEEP_WEEKLY" --keep-monthly="$BORG_KEEP_MONTHLY" "$BACKUP_PATH"
+    handle_borg_rc $? "Prune ${pattern}"
+}
+
+# Chargement variables environnement
+log "STARTING BACKUP JOB"
+
+[ -f /tmp/project_env.sh ] && source /tmp/project_env.sh
+
+KUMA_PUSH_URL="${KUMA_PUSH_URL:-}"
+
+#########
+# MYSQL #
+#########
+if [ -n "${MYSQL_HOST:-}" ] && [ -n "${MYSQL_USER:-}" ] && [ -n "${MYSQL_PASSWORD:-}" ]; then
+    log "STARTING BACKUP MYSQL"
+    MYSQL_DUMP_FILE="/tmp/all_databases_mysql.sql"
+    MYSQL_PWD="$MYSQL_PASSWORD" mysqldump --all-databases --single-transaction -u "$MYSQL_USER" -h "$MYSQL_HOST" > "$MYSQL_DUMP_FILE"
+
     RC=$?
-    if [ $RC -ne 0 ];then
+    if [ $RC -ne 0 ]; then
+        log "ERROR: MySQL dump failed (rc=$RC)"
         BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
     else
-        borgbackup create --stats --compression lz4 "$BACKUP_PATH"::db_mysql_$(date +%Y-%m-%d_%H:%M) "/tmp/all_databases_mysql.sql"
-        RC=$?
-        [ $RC -ne 0 ] && BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
+        backup_archive "db_mysql_$(date +%Y-%m-%d_%H:%M)" "$MYSQL_DUMP_FILE"
     fi
+    rm -f "$MYSQL_DUMP_FILE"
 
-    rm /tmp/all_databases_mysql.sql
-    echo $(date --iso-8601=seconds) STARTING PRUNE MYSQL
-    borgbackup prune --stats -v --glob-archives='db_mysql_*' --keep-within="$BORG_KEEP_WITHIN" --keep-weekly="$BORG_KEEP_WEEKLY" --keep-monthly="$BORG_KEEP_MONTHLY" "$BACKUP_PATH"
-    RC=$?
-    [ $RC -ne 0 ] && BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
+    log "STARTING PRUNE MYSQL"
+    prune_archive "db_mysql_*"
 fi
 
-if ! ([ -z "$POSTGRES_HOST" ] || [ -z "$POSTGRES_USER" ] || [ -z "$POSTGRES_PASSWORD" ]); then
-    echo $(date --iso-8601=seconds) STARTING BACKUP POSTGRES
-    PGPASSWORD="$POSTGRES_PASSWORD" pg_dumpall -U "$POSTGRES_USER" -h "$POSTGRES_HOST" > /tmp/all_databases_pg.out
+############
+# POSTGRES #
+############
+if [ -n "${POSTGRES_HOST:-}" ] && [ -n "${POSTGRES_USER:-}" ] && [ -n "${POSTGRES_PASSWORD:-}" ]; then
+    log "STARTING BACKUP POSTGRES"
+    PG_DUMP_FILE="/tmp/all_databases_pg.out"
+    PGPASSWORD="$POSTGRES_PASSWORD" pg_dumpall -U "$POSTGRES_USER" -h "$POSTGRES_HOST" > "$PG_DUMP_FILE"
+
     RC=$?
-    if [ $RC -ne 0 ];then
+    if [ $RC -ne 0 ]; then
+        log "ERROR: PostgreSQL dump failed (rc=$RC)"
         BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
     else
-        borgbackup create --stats --compression lz4 "$BACKUP_PATH"::db_postgres_$(date +%Y-%m-%d_%H:%M) "/tmp/all_databases_pg.out"
-        RC=$?
-        [ $RC -ne 0 ] && BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
+        backup_archive "db_postgres_$(date +%Y-%m-%d_%H:%M)" "$PG_DUMP_FILE"
     fi
+    rm -f "$PG_DUMP_FILE"
 
-    rm /tmp/all_databases_pg.out
-    echo $(date --iso-8601=seconds) STARTING PRUNE POSTGRES
-    borgbackup prune --stats -v --glob-archives='db_postgres_*' --keep-within="$BORG_KEEP_WITHIN" --keep-weekly="$BORG_KEEP_WEEKLY" --keep-monthly="$BORG_KEEP_MONTHLY" "$BACKUP_PATH"
-    RC=$?
-    [ $RC -ne 0 ] && BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
+    log "STARTING PRUNE POSTGRES"
+    prune_archive "db_postgres_*"
 fi
 
+###########
+# FOLDERS #
+###########
 for d in "$FOLDERS_TO_BACKUP_PATH"/*; do
-    if [ -d "$d" ]; then
-        echo $(date --iso-8601=seconds) STARTING BACKUP FOLDER "$d"
-        borgbackup create --stats --compression lz4 "$BACKUP_PATH"::${d##*/}_$(date +%Y-%m-%d_%H:%M) "$d"
-	RC=$?
-        [ $RC -ne 0 ] && BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
+    [ ! -d "$d" ] && continue
+    folder_name="${d##*/}"
+    log "STARTING BACKUP FOLDER $d"
+    backup_archive "${folder_name}_$(date +%Y-%m-%d_%H:%M)" "$d"
 
-        echo $(date --iso-8601=seconds) STARTING PRUNE FOLDER "$d"
-        borgbackup prune --stats -v --glob-archives="${d##*/}_*" --keep-within="$BORG_KEEP_WITHIN" --keep-weekly="$BORG_KEEP_WEEKLY" --keep-monthly="$BORG_KEEP_MONTHLY" "$BACKUP_PATH"
-	RC=$?
-        [ $RC -ne 0 ] && BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
-    fi
+    log "STARTING PRUNE FOLDER $d"
+    prune_archive "${folder_name}_*"
 done
 
-echo $(date --iso-8601=seconds) COMPACTING BACKUP
-borgbackup compact "$BACKUP_PATH"
-RC=$?
-[ $RC -ne 0 ] && BACKUP_ERRORS=$((BACKUP_ERRORS + 1))
+###########
+# COMPACT #
+###########
 
+log "COMPACTING BACKUP"
+
+borgbackup compact "$BACKUP_PATH"
+handle_borg_rc $? "Compact repository"
+
+########
+# KUMA #
+########
 if [ -n "$KUMA_PUSH_URL" ]; then
-    echo $(date --iso-8601=seconds) SEND MONITORING NOTIFICATION
+    log "SEND MONITORING NOTIFICATION"
     if [ "$BACKUP_ERRORS" -eq 0 ]; then
-        curl -fsS -m 10 --retry 3 "${KUMA_PUSH_URL}?status=up&msg=OK">/dev/null 2>&1 || true
+        curl -fsS -m 10 --retry 3 "${KUMA_PUSH_URL}?status=up&msg=OK" > /dev/null 2>&1 || true
     else
-        curl -fsS -m 10 --retry 3 "${KUMA_PUSH_URL}?status=down&msg=failed%20with%20${BACKUP_ERRORS}%20errors">/dev/null 2>&1 || true
+        curl -fsS -m 10 --retry 3 "${KUMA_PUSH_URL}?status=down&msg=failed%20with%20${BACKUP_ERRORS}%20errors" > /dev/null 2>&1 || true
     fi
 fi
 
-echo $(date --iso-8601=seconds) END BACKUP JOB WITH ${BACKUP_ERRORS} ERRORS
+log "END BACKUP JOB WITH ${BACKUP_ERRORS} ERRORS"
+
+exit "$BACKUP_ERRORS"
